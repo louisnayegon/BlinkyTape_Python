5951074afddb5e20b0d07827da4bb72d728b40c0
Louis Henry Nayegon <louis.henry.nayegon@gmail.com>
2014-11-28 13:25:13 +0000

    List tags

---
+++ web/main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from twitter_tags import twitter_tags
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/edit/", EditHandler),
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+            autoescape=None
+            )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+            "index.html",
+            twitter_tags=twitter_tags
+        )
+
+
+class EditHandler(tornado.web.RequestHandler):
+    def get(self):
+        try:
+            tag = self.get_argument('tag')
+            self.render(
+                "edit.html",
+                twitter_tags=twitter_tags,
+                tag=tag
+            )
+        # will do some searching
+        except AssertionError:
+            self.write("no params")
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
